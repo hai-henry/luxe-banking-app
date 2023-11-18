4cddd4d740fc19eae84713ff5b4ce53022bbb209
hai-henry <hvnryho@gmail.com>
2023-11-17 23:08:26 -0600

    Update name convention

--- src/containers/info/Info.tsx
+++ src/containers/info/Info.tsx
@@ -8,16 +8,30 @@ const Info = () => {
       <div className="info__content">
         <div className="info__content-left">
           <div className="info__graph-container">
-            <div className="info__net-worth-graph">
+            <div className="info__graph">
               <img src={networth_graph} alt="Net worth Graph" />
-              <div className="info__net-worth-graph-values">
+              <div className="info__graph-values">
                 <p>$128,000</p>
                 <p>$100,000</p>
               </div>
             </div>
-            <div className="info__net-worth-graph-months">
+            <div className="info__graph-months">
               <p>Oct 14</p>
               <p>Nov 14</p>
+            </div>
+          </div>
+          <div className="info__net-worth-container">
+            <div className="info__account">
+              <p>INDIVIDUAL</p>
+              <p>LX74897</p>
+            </div>
+            <div className="info__amount">
+              <p>Net Worth</p>
+              <p>$128,798.60</p>
+            </div>
+            <div className="info__change">
+              <div className="info__change-dollar"></div>
+              <div className="info__change-percent"></div>
             </div>
           </div>
         </div>
