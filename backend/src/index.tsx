fbc86eb0e0124d5d95708fc05f0e1719c00ef02e
hai-henry <henry.h.ho-1@ou.edu>
2023-11-26 21:44:15 -0600

    Start backend

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -1,1 +1,26 @@
+"use strict";
 // TODO: Add Plaid API
+require("dotenv").config;
+
+const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
+const util = require("util");
+const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const cors = require("cors");
+
+const APP_PORT = process.env.APP_PORT || 8000;
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+      "PLAID-SECRET": PLAID_SECRET,
+    },
+  },
+});
