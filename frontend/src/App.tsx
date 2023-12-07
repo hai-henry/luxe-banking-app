e350b60d14f6487930f38139257a462d03d4f734
hai-henry <henry.h.ho-1@ou.edu>
2023-12-07 00:10:20 -0600

    TODO: Get accounts using access token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,31 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:8000/' // Base for HTTP Request
 
+// TODO: Get Accounts using access token
+function GetAccounts({ publicToken }: { publicToken: string }) {
+  useEffect(() => {
+    async function fetchAccessToken() {
+      try {
+        const accessToken = await axios.post('/api/exchange_public_token', {
+          public_token: publicToken, // Ensure the property name matches 'public_token'
+        })
+
+        console.log('Access Token huh: ', accessToken)
+        const accounts = await axios.post('/api/accounts', {
+          accessToken: accessToken.data.access_token,
+        })
+
+        console.log('Accounts: ', accounts.data)
+      } catch (error) {
+        console.error('Error: ', error)
+      }
+    }
+    fetchAccessToken()
+  }, [])
+
+  return <></>
+}
+
 function App() {
   return (
     <div className="App">
@@ -13,6 +38,7 @@ function App() {
         <Navbar />
         <Header />
         <Info />
+        <GetAccounts publicToken="public-sandbox-4b9b9b9b-7b9b-4b9b-9b9b-9b9b9b9b9b9b" />
       </div>
     </div>
   )
